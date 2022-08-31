1c6c4c646d794bee5057f1545075efde32d1b0a2
RiyaPatel288 <patel.riya3009@gmail.com>
2022-08-31 16:07:30 +0530

    initial commit

---
+++ src/pages/blogList.js
@@ -0,0 +1,14 @@
+
+import { Button } from 'react-bootstrap';
+import Navigation from '../component/navigation';
+
+const BlogList = () => {
+    return (
+      <div className="App">
+        <Navigation/>
+       
+      </div>
+    );
+  }
+
+export default BlogList;
